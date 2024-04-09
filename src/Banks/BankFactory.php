32a1d252e5e3448a6372cd39f42c31fdf86e6560
Kevin van Hulst <kevin.van.hulst@mailtopay.nl>
2024-04-09 09:39:02 +0200

    add BankFactory.php

---
+++ src/Banks/BankFactory.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace POM\iDEAL\Banks;
+
+class BankFactory
+{
+    /** Creates a Bank class based on bank codes
+     *
+     * @param string $bankcode
+     * @return BankInterface
+     */
+    public static function bankFromString(string $bankcode): BankInterface
+    {
+        return match ($bankcode){
+            'abn' => new ABN(),
+            'bng' => new BNG(),
+            'bnp' => new BNP(),
+            'db' => new DB(),
+            'ing' => new ING(),
+            'rabo' => new RABO(),
+            'worldline' => new Worldline(),
+        };
+    }
+}
