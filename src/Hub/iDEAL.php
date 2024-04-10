af0d11ed2f8b5cf33bb2b1537388b0ba4d18ba95
Kevin van Hulst <kevin.van.hulst@mailtopay.nl>
2024-04-10 15:47:15 +0200

    add callback verification method

--- src/Hub/iDEAL.php
+++ src/Hub/iDEAL.php
@@ -2,11 +2,14 @@
 
 namespace POM\iDEAL\Hub;
 
+use Exception;
+use Firebase\JWT\JWT;
 use POM\iDEAL\Exceptions\IDEALException;
 use POM\iDEAL\Hub\Requests\AccessTokenRequest;
 use POM\iDEAL\Hub\Requests\TransactionRequest;
 use POM\iDEAL\Hub\Requests\TransactionStatusRequest;
 use POM\iDEAL\Hub\Resources\AccessToken;
+use stdClass;
 
 readonly final class iDEAL
 {
@@ -111,5 +114,53 @@ readonly final class iDEAL
     public function getAccessToken(): AccessToken
     {
         return $this->accessToken;
+    }
+
+    /**
+     * Verify the callback Currence sends to a webhook
+     *
+     * @param string $callbackResponse raw json body
+     * @param array|null $headers
+     * @return bool
+     */
+    public function verifyCallbackResponse(string $callbackResponse, array $headers = null): bool
+    {
+        if (is_null($headers)) {
+            $requestId  = $_SERVER['HTTP_REQUEST_ID'] ?? '';
+            $sender     = $_SERVER['HTTP_X_SENDER'] ?? '';
+            $signature  = $_SERVER['HTTP_SIGNATURE'] ?? '';
+        } else {
+            $requestId  = $headers['Request-Id'] ?? '';
+            $sender     = $headers['X-Sender'] ?? '';
+            $signature  = $headers['Signature'] ?? '';
+        }
+
+        if (empty($requestId) || empty($signature)) {
+            return false;
+        }
+
+        if ($sender !== 'iDEAL') {
+            return false;
+        }
+
+        $signatureWithPayload = str_replace('..', '.'.JWT::urlsafeB64Encode($callbackResponse).'.', $signature);
+
+        $jwtHeaders = new stdClass();
+
+        try {
+            JWT::decode(
+                $signatureWithPayload,
+                $this->certificateStore->getCertificates(),
+                $jwtHeaders,
+            );
+        } catch (Exception) {
+            return false;
+        }
+
+        if ($requestId !== $jwtHeaders->{'https://idealapi.nl/jti'}) {
+            return false;
+        }
+
+        return true;
     }
 }
